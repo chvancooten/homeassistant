d6a4e27401f8d0cda1df0b1ac655773afb67c522
van Cooten <cvancooten@deloitte.nl>
2020-04-26 13:14:34 +0200

    Adapted config to new network structure

--- appdaemon/apps/partyMode.py
+++ appdaemon/apps/partyMode.py
@@ -10,11 +10,11 @@ class partyMode(hass.Hass):
     def motion(self, entity, attribute, old, new, kwargs):
         self.log("Party engaged!")
         self.call_service("media_player/play_media", entity_id="media_player.tonecas", media_content_type="music",
-            media_content_id="http://192.168.1.100:8123/local/audio/running.mp3")
+            media_content_id="http://192.168.2.100:8123/local/audio/running.mp3")
         self.call_service("light/turn_off", entity_id="all")
         time.sleep(2)
         self.call_service("media_player/play_media", entity_id="media_player.tonecas", media_content_type="music",
-            media_content_id="http://192.168.1.100:8123/local/audio/running.mp3")
+            media_content_id="http://192.168.2.100:8123/local/audio/running.mp3")
         self.call_service("light/turn_on", entity_id="light.nanoleaf", brightness=255, effect="80s Burst Synth")
         time.sleep(1.6)
         self.call_service("light/turn_on", entity_id="light.livingroom", brightness=255, effect="Strobe color")
